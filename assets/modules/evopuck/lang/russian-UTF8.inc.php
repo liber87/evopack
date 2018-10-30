4efb35d60e3e3806e5986a8dd629f9514bd42cfb
ProjectSoft <projectsoft2009@yandex.ru>
2018-10-30 12:51:53 +0400

    Added language support

---
+++ assets/modules/evopuck/lang/russian-UTF8.inc.php
@@ -0,0 +1,13 @@
+<?
+$_lang['evopuck_module_title'] = 'Модуль создания установочных пакетов';
+$_lang['evopuck_module_h1'] = 'Создание установочных пакетов';
+$_lang['evopuck_module_main'] = 'Основное';
+$_lang['evopuck_module_build_package'] = 'Создание пакета';
+$_lang['evopuck_module_formed_packages'] = 'Сформированные пакеты';
+$_lang['evopuck_module_enter_package_name'] = 'Введите название пакета';
+$_lang['evopuck_module_build_a_package'] = 'Сформировать пакет';
+$_lang['evopuck_module_no_formated_package'] = 'Сформированных пакетов нет';
+
+// Dev Out
+$_lang['evopuck_module_devout_1'] = 'Пока только содержимое...(';
+?>
