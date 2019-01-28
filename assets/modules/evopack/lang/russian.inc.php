22ecb9ce0a29350b6bfff68b6ca4a115218e40d0
liber87 <bd87@bk.ru>
2019-01-29 00:14:21 +0300

    Update russian.inc.php

--- assets/modules/evopack/lang/russian.inc.php
+++ assets/modules/evopack/lang/russian.inc.php
@@ -1,13 +1,13 @@
 <?
-$_lang['evopuck_module_title'] = 'Модуль создания установочных пакетов';
-$_lang['evopuck_module_h1'] = 'Создание установочных пакетов';
-$_lang['evopuck_module_main'] = 'Основное';
-$_lang['evopuck_module_build_package'] = 'Создание пакета';
-$_lang['evopuck_module_formed_packages'] = 'Сформированные пакеты';
-$_lang['evopuck_module_enter_package_name'] = 'Введите название пакета';
-$_lang['evopuck_module_build_a_package'] = 'Сформировать пакет';
-$_lang['evopuck_module_no_formated_package'] = 'Сформированных пакетов нет';
+$_lang['evopack_module_title'] = 'Модуль создания установочных пакетов';
+$_lang['evopack_module_h1'] = 'Создание установочных пакетов';
+$_lang['evopack_module_main'] = 'Основное';
+$_lang['evopack_module_build_package'] = 'Создание пакета';
+$_lang['evopack_module_formed_packages'] = 'Сформированные пакеты';
+$_lang['evopack_module_enter_package_name'] = 'Введите название пакета';
+$_lang['evopack_module_build_a_package'] = 'Сформировать пакет';
+$_lang['evopack_module_no_formated_package'] = 'Сформированных пакетов нет';
 
 // Dev Out
-$_lang['evopuck_module_devout_1'] = 'Пока только содержимое...(';
+$_lang['evopack_module_devout_1'] = 'Пока только содержимое...(';
 ?>
